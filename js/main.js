4096ecaa5f6c3ff66c337a302e8e09385e0cf304
core.editor <andreeavillatte@gmail.com>
2019-04-12 15:39:56 +0200

    ajout commentaires

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-// je stocke mes boutons dans le tableau "buttons"
+// creer le tableau avec 3 boutons en les choissisant apres leur classe 
 var buttons=document.getElementsByClassName('color');
-//le bouton 1 change le texte en vert
-buttons[0].addEventListener('click',function(){document.getElementById('text').style.color="green"});
-//le bouton 2 change le texte en rouge
-buttons[1].addEventListener('click',function(){document.getElementById('text').style.color="red"});
-//le bouton 3 change le texte en bleu
-buttons[2].addEventListener('click',function(){document.getElementById('text').style.color="blue"});
+// creer la fonction qui va commencer a travailler dès qu'on click sur le button,
+// et va changer la couleur de mon text en fonction de la couleur choissise.
+// Pour vert ça va etre le premier bouton [0, parce que la numerotation commence par 0 et pas par 1], 
+// pour red ca va etre le deuxieme bouton
+// pour bleu ca va etre le troisieme bouton 
+buttons[0].addEventListener('click', function () {
+    document.getElementById('text').style.color = 'green'
+});
+buttons[1].addEventListener('click', function () {
+    document.getElementById('text').style.color = 'red'
+});
+buttons[2].addEventListener('click', function () {
+    document.getElementById('text').style.color='blue'
+});
+         
